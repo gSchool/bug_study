57722f2094c33505543897be786b2279be0bd91d
Roberto Ortega <berto.ort@gmail.com>
2017-01-16 13:54:23 -0700

    Tweak errors for lecture

--- bugs.js
+++ bugs.js
@@ -1,16 +1,16 @@
 //dynamically get the year and add to the footer
 
 $(document).ready(function(){
-  var current_year = new Date().getFullYear();
+  var currentYear = new Date().getFullYear();
   $("#year").html(currentyear);
 
   var bug_list = ["butterfly", "ladybug", "mosquito", "pincher", "mantis"];
 
-  var random_bug_number = Math.floor(Math.random() * bug_list.length) + 1;
-  $("#author").html(bug_list[random_bug_number]);
+  var random_bug_number = Math.floor(Math.random() * bug_list.length()) + 1;
+  var author = bug_list[random_bug_number];
+  $("#author").html(author);
 
   for (var i = 0; i < bug_list.length; i++) {
     $("#bug_display").append("<li>"+bug_list[i]+"</li>");
-  
 
 })
